f5d2739fb8dbecbc9dd736477797014030a37449
PCBiS <anycast24@gmail.com>
2020-04-08 17:29:16 +0900

    Update FirstJava.java

--- java_workspace/FirstJAVA/src/chapter01/FirstJava.java
+++ java_workspace/FirstJAVA/src/chapter01/FirstJava.java
@@ -1,9 +1,12 @@
 package chapter01;
 
 public class FirstJava {
-
 	public static void main(String[] args) {
 		System.out.println("Hello, Java!");
+		sum(10, -3);
+	}	
+	
+	public static void sum(int a, int b) {
+		System.out.println(a + b);
 	}
-
 }
