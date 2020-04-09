df3f284651f8baded51c9d1a2b754ebc2e337df0
PCBiS <anycast24@gmail.com>
2020-04-09 11:55:53 +0900

    12시 전 저장

--- java_workspace/Exam/src/chapter02/Ex01.java
+++ java_workspace/Exam/src/chapter02/Ex01.java
@@ -1,5 +1,6 @@
+package chapter02;
 
-public class ex01 {
+public class Ex01 {
 	public static void main(String[] args) {		
 		System.out.println("2+5=" + 2 + 5);
 		/*
---
+++ java_workspace/Exam/src/chapter02/Ex03.java
@@ -0,0 +1,13 @@
+package chapter02;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		int num1;
+		num1 = 10;
+		System.out.println("num1 value : " + num1);
+		int num2 = 20;
+		System.out.println("num2 value : " + num2);
+		int num3 = num1 + num2;
+		System.out.println("num1: " + num1 + " + num2: " + num2 + " = " + "num3: " + num3);
+	}
+}
