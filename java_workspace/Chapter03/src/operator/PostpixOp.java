05a1c66069c6f3a075b4e23bb99b9d3021d0ea74
PCBiS <anycast24@gmail.com>
2020-04-10 16:02:28 +0900

    111

--- java_workspace/Chapter03/src/operator/PostpixOp.java
+++ java_workspace/Chapter03/src/operator/PostpixOp.java
@@ -2,13 +2,13 @@ package operator;
 
 public class PostpixOp {
 	//집에가기 전에 이유 스스로 풀어 써놓기
+	//후위 단항 연산자 특성 - 값을 대입 한 후 연산함.
 	public static void main(String[] args) {
-		int num1= 7;		// 
+		int num1= 7;		// 초기 값은 num = 7
 		int num2 = num1++;	// 
 		int num3 = num1--;	// 
 		
 		System.out.println(num1++); // 출력 : 7,
-								 	// 			
 		System.out.println(num2);	// 출력 : 7, 
 		System.out.println(num2--); // 출력 : 7,
 		System.out.println(num3);	// 출력 : 8,
--- java_workspace/Chapter03/src/operator/PrefixOP.java
+++ java_workspace/Chapter03/src/operator/PrefixOP.java
@@ -2,8 +2,9 @@ package operator;
 
 public class PrefixOP {
 	//집에가기 전에 이유 스스로 풀어 써놓기
+	//전위 단항 연산자 특성 - 대입하기 전에 연산 된 값을 대입함.  
 	public static void main(String[] args) {
-		int num1= 7;		// 
+		int num1= 7;		// 초기 값은 num = 7
 		int num2 = ++num1;	// 
 		int num3 = --num1;	// 
 		
