0181c553355e33844809e8814ac3e7ca3bf3dd5c
PCBiS <anycast24@gmail.com>
2020-04-10 18:24:45 +0900

    집에 가기 전 저장
    연산자 쳅터의 문제 풀이까지 완료하였음.

---
+++ java_workspace/home_work/src/operator_homework/Answer03_Minimum_Calc.java
@@ -0,0 +1,13 @@
+package operator_homework;
+
+public class Answer03_Minimum_Calc {
+	public static void main(String[] args) {
+		int a, b, c;
+		a = 3+6; // a는 3+6의 결과를 대입
+		System.out.println("3+6="+a); // a = 9
+		b = a+9; // a + 9의 결과를 b에 대입
+		System.out.println("3+6+9="+b); // b = 18
+		c = b+12; // b + 12 의 결과를 c에 대입
+		System.out.println("3+6+9+12="+c); // c = 30
+	}
+}
