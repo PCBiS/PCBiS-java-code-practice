d450ee773b8734ed3829a1018bd2fc3dfc58b97a
PCBiS <anycast24@gmail.com>
2020-04-18 17:34:12 +0900

    토요일 객체지향 프로그래밍 연습
    1

---
+++ java_workspace/home_work/src/home_project01/Phone.java
@@ -0,0 +1,18 @@
+package home_project01;
+
+public class Phone {
+	public static void main(String[] args) {
+		Phoneinfor in1 = new Phoneinfor();
+		Phoneinfor in2 = new Phoneinfor();
+		Phoneinfor in3 = new Phoneinfor();
+		
+		in1.numberAdd("김철수", "010-1234-5778");
+		in2.numberAdd("박영희", "010-3332-7682", "19900926");
+		in3.numberAdd("이영호", "010-5144-6771");
+		System.out.println("");
+		
+		in1.showPbook();
+		in2.showPbook();
+		in3.showPbook();		
+	}
+}
